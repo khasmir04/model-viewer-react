0d024360ce0ed8dcd6573156a4ef79d7108905f2
Khasmir Jhon Caluscusao <61497334+khasmir04@users.noreply.github.com>
2021-10-04 15:09:59 +0800

    Added responsiveness on logo and title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-import './Custom.css';
 import './index.css';
+import './Custom.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 // import ModelViewerObj from './components/ModelViewerObj';
@@ -22,10 +22,10 @@ function App() {
       <section id="main" className="container mx-auto h-[calc(100vh-160px)] text-center">
         <div className="flex mx-auto w-6/12 items-center pt-6 mb-6">
           <div
-            className={"hover:bg-white hover:text-black border-2 text-center text-xl text-white bg-gray-400 border-gray-400 cursor-pointer w-full tab " + (activeTab === "ship" ? 'active' : '')}
+            className={"hover:bg-white hover:text-black border-2 text-center text-xs md:text-xl text-white bg-gray-400 border-gray-400 cursor-pointer w-full tab " + (activeTab === "ship" ? 'active' : '')}
             onClick={() => switchTab("ship")}>Ship Type</div>
           <div
-            className={"hover:bg-white hover:text-black border-2 text-center text-xl text-white bg-gray-400 border-gray-400 cursor-pointer w-full tab " + (activeTab === "area" ? 'active' : '')}
+            className={"hover:bg-white hover:text-black border-2 text-center text-xs md:text-xl text-white bg-gray-400 border-gray-400 cursor-pointer w-full tab " + (activeTab === "area" ? 'active' : '')}
             onClick={() => switchTab("area")}>Exercise Area</div>
         </div>
         <div className="flex justify-center">
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ export default function Header() {
   return (
     <>
       <div className="relative">
-        <img src="/assets/img/logo.png" alt="Terp Logo" className="max-h-20 absolute"></img>
+        <img src="/assets/img/logo.png" alt="Terp Logo" className="max-h-10 md:max-h-20 absolute top-5 md:top-0"></img>
         <div className="w-full h-20 bg-gray-400 flex items-center justify-center">
-          <h1 className="section-title text-3xl">TERP Content 3D Model Viewer</h1>
+          <h1 className="section-title text-lg md:text-3xl">TERP Content 3D Model Viewer</h1>
         </div>
       </div>
     </>
